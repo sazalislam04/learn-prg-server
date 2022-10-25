8250eb0dbbebc32069b65e3e52a140972de970e9
sazalislam04 <mdsazalislam04@gmail.com>
2022-10-25 03:02:53 -0600

    setup courses server

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 app.use(cors());
+
+app.get("/", (req, res) => {
+  console.log("Online Education Website Server is running");
+});
+
+app.listen(port, () => {
+  console.log(`servier is running on port ${port}`);
+});
